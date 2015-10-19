aa12e682506554b11ec4a3ef96cbd0fa08bf8c3d
unknown <galini@galini-zen.redmond.corp.microsoft.com>
2015-10-18 18:42:25 -0700

    add deployment templates

---
+++ Deployment/createsite.ps1
@@ -0,0 +1,22 @@
+﻿$siteName = "Galin-BakeryInstall2"
+$resourceGroupName = "AppServiceDemo"
+$rglocation = "East US"
+$location = "North Europe"
+$hostingPlanName = "GalinDemo"
+$bakeryDatabase = "Data Source=f1gov0l546.database.windows.net;Initial Catalog=bakery;User Id=demo;Password=xxxxxxxxx;"
+$bakeryDatabaseStaging = "Data Source=f1gov0l546.database.windows.net;Initial Catalog=bakery_staing;User Id=demo;Password=xxxxxxx;"
+ 
+cls
+New-AzureResourceGroup -Name $resourceGroupName -Location $rglocation -TemplateFile .\WebAppLinkedToGithub.json `
+                        -hostingPlanName $hostingPlanName -siteName $siteName -siteLocation $location -Force  `
+                        -bakeryDatabase $bakeryDatabase -bakeryDatabaseStaging $bakeryDatabaseStaging -sku Standard -workerSize 2
+
+<#
+cls
+New-AzureResourceGroupDeployment -Name $resourceGroupName -Location $rglocation -TemplateFile .\WebAppLinkedToGithub.json - `
+                        -hostingPlanName $hostingPlanName -siteName $siteName -siteLocation $location -Force  `
+                        -bakeryDatabase $bakeryDatabase -sku Standard -workerSize 2
+
+#>
+ 
+ 
---
+++ Deployment/helper.ps1
@@ -0,0 +1,26 @@
+﻿cd C:\projects\Templates
+
+$ErrorActionPreference= "stop"
+Import-Module "C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ServiceManagement\Azure\Azure.psd1"
+$VerbosePreference="continue"
+Switch-AzureMode AzureResourceManager
+# Switch-AzureMode AzureServiceManagement
+Add-AzureAccount 
+Select-AzureSubscription antps05
+
+
+
+
+# ARMClient.exe GET  https://management.azure.com/subscriptions/0d3ae56c-deaf-4982-b514-33d016d4a683/resourceGroups/GalinTests/providers/Microsoft.Web/sites/Galin-WebJobsSite/config/slotConfigNames?api-version=2014-06-01
+
+<#
+ARMClient.exe POST https://management.azure.com/subscriptions/0d3ae56c-deaf-4982-b514-33d016d4a683/resourceGroups/GalinTests/providers/Microsoft.Web/sites/galin-swap3/slots/slot2/slotsswap?api-version=2014-06-01 "{'targetSlot': 'slot1'}" -verbose
+ARMClient.exe GET https://management.azure.com//subscriptions/0d3ae56c-deaf-4982-b514-33d016d4a683/resourceGroups/GalinTests/providers/Microsoft.Web/sites/galin-swap3/slots/slot2/operations/3d003b27-8f7e-4ba0-b322-d49c4d782bf8?api-version=2014-06-01 -verbose
+
+
+ARMClient.exe POST https://management.azure.com/subscriptions/0d3ae56c-deaf-4982-b514-33d016d4a683/resourceGroups/GalinTests/providers/Microsoft.Web/sites/TestSticky6/config/appsettings/list?api-version=2014-06-01 -verbose
+ARMClient.exe GET https://management.azure.com/subscriptions/0d3ae56c-deaf-4982-b514-33d016d4a683/resourceGroups/GalinTests/providers/Microsoft.Web/sites/TestSticky6/config/slotconfignames?api-version=2014-06-01 -verbose
+#> 
+
+
+
